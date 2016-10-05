0c7c2f04cb691a09e97249fbea21b3d29ba01ee9
venkatess <venkateshmurthy91@gmail.com>
2016-10-05 18:34:07 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article one', function (req,res){
+app.get('/article-one', function (req,res){
     res.send('Article one is requested');
     
 });
-app.get('/article two', function (req,res){
+app.get('/article-two', function (req,res){
     res.send('Article two is requested');
 });
-app.get('/article three', function (req,res){
+app.get('/article-three', function (req,res){
     res.send('Article three is requested');
 });
 app.get('/ui/style.css', function (req, res) {
