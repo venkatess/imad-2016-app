8419e729e053f9b355cc94219a1f317b619c06ab
venkatess <venkateshmurthy91@gmail.com>
2016-10-05 20:25:59 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res){
-    res.send(createTemplate(articleOone));
+    res.send(createTemplate(articleOne));
     
 });
 app.get('/article-two', function (req,res){
