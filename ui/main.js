97f0717d4ce6cb90de4c2e59b75001a4a66909d3
venkatess <venkateshmurthy91@gmail.com>
2016-10-23 18:22:19 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//Changing the content
 var element=document.getElementById('main-text');
 element.innerHTML='Dallas';
+//Changing the image
+var kodi=document.getElementById('main-text');
+kodi.onclick=function(){
+    kodi.style.marginLeft='100px';
+};
