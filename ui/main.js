93448b92d51da6d2b28a63df56bb3e799b084901
venkatess <venkateshmurthy91@gmail.com>
2016-10-23 20:34:14 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,24 @@
 var button=document.getElementById('counter');
 button.onclick=function(){
-  //Make request to counter end point
-  
+  //Create request object
+  var request=new XMLHttpRequest();
   //Capture the response and stored it in avariable
-  
+  request.onreadystatechange=function(){
+   if(request.readystate==XMLHttpRequest.DONE){
+       //Take action
+       if (request.status==200){
+           var counter=request.responseText;
+           var span=document.getElementById('count');
+           span.innerHTML=counter.toString();
+       }
+   }   
+  };
   
   //Render the variable to display
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+  //Make request
+  request.open('GET','http://venkatess.imad.hasura-app.io',true);
+  request.send(null);
+  
+  
+  
 };
