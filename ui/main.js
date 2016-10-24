8c2b8cc9817bc6ae46f5afc73710b84d0a18d331
venkatess <venkateshmurthy91@gmail.com>
2016-10-23 20:09:36 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,6 @@
-console.log('Loaded!');
-//Changing the content
-var element=document.getElementById('main-text');
-element.innerHTML='Dallas';
-//Changing the image Moving image
-var kodi=document.getElementById('tang');
-var marginLeft=0;
-function moveRight(){
- marginLeft-marginLeft + 10; 
- kodi.style.marginLeft=marginLeft + 'px';
-}
-kodi.onclick=function(){
-   var interval=setInterval(moveRight,100);
+var button=document.getElementById('counter');
+button.onclick=function(){
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
 };
